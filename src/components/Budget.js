0ecd210245738a29d8b4911d766fd06cbc5f3e36
Eren Kaan ÇAKIR <102954021+kaancakr@users.noreply.github.com>
2024-03-20 23:10:18 +0300

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,41 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget, remaining } = useContext(AppContext);
+    const { Location } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        const inputValue = event.target.value;
+        
+        // Validate if input is a number and within the upper limit
+        if (!isNaN(inputValue) && parseInt(inputValue) <= 20000) {
+            const newBudgetValue = parseInt(inputValue);
+            
+            // Check if new budget value does not exceed remaining budget
+            if (newBudgetValue <= remaining) {
+                setNewBudget(newBudgetValue);
+            } else {
+                alert("The new budget exceeds the remaining budget!");
+            }
+        } else {
+            alert("Please enter a valid number up to 20,000 for the budget.");
+        }
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: {Location}{budget}</span>
+            <input 
+                type="number" 
+                step="10" 
+                value={newBudget} 
+                onChange={handleBudgetChange}
+            />
+        </div>
+    );
+};
+
+export default Budget;
 
